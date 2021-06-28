8b9f5974643ed18ff5d42eb232c134d464219fcf
Gerico Vidanes <gericov@outlook.com>
2021-06-28 21:40:31 +0100

    Multiple item inputs in Dash UI

--- planner_ui.py
+++ planner_ui.py
@@ -16,7 +16,7 @@ cyto.load_extra_layouts()
 app.layout = html.Div([
     html.H1("Process Network"),
     html.Div(["Item: ",
-        dcc.Input(id='item_input', value='smart_plating', type='text')
+        dcc.Input(id='item_inputs', value='smart plating', type='text')
     ]),
     cyto.Cytoscape(
         id='process_network',
@@ -54,9 +54,9 @@ app.layout = html.Div([
     Output(component_id='process_network', component_property='elements'),
     Output(component_id='process_network', component_property='layout'),
     Output(component_id='raw_materials', component_property='children'),
-    Input(component_id='item_input', component_property='value')
+    Input(component_id='item_inputs', component_property='value')
 )
-def update_graph(item_name):
+def update_graph(item_names):
     elements = []
     layout = {
         'name': 'breadthfirst',
@@ -64,54 +64,57 @@ def update_graph(item_name):
     }
     mats = []
 
-    if len(item_name) > 0:
+    if len(item_names) > 0:
         # Load item data
         with open('asset_data.pickle', 'rb') as infile:
             asset_data = pickle.load(infile)
 
-        item_name = item_name.replace(' ','_').lower()
+        planner = ProcessGraph(asset_data)
 
-        # Check if valid item
-        if item_name in asset_data:
+        # Split up item requests
+        names = item_names.split(',')
+        for item_name in names:
+            item_name = item_name.replace(' ','_').lower()
+            
+            # Check if valid item
+            if item_name in asset_data:
 
-            planner = ProcessGraph(asset_data)
+                planner.add_request(item_name, 1)
 
-            planner.add_request(item_name, 1)
+                # layout['roots'] = [node for node in planner.root_nodes]
+                layout['roots'].append(f'{item_name}_OUT')
 
-            # layout['roots'] = [node for node in planner.root_nodes]
-            layout['roots'].append(f'{item_name}_OUT')
+                # Get nodes in graph
+                for node_name in planner.graph_nodes:
+                    node = planner.graph_nodes[node_name]
+                    if isinstance(node,ItemNode):
+                        label = f"{round(node.rate_filled,1)}/{round(node.rate_requested,1)} {node.name} per min"
+                    elif isinstance(node,BuildingNode):
+                        label = f"{node.name} ({round(node.clock_speed*100,1)}%)"
 
-            # Get nodes in graph
-            for node_name in planner.graph_nodes:
-                node = planner.graph_nodes[node_name]
-                if isinstance(node,ItemNode):
-                    label = f"{round(node.rate_filled,1)}/{round(node.rate_requested,1)} {node.name} per min"
-                elif isinstance(node,BuildingNode):
-                    label = f"{node.name} ({round(node.clock_speed*100,1)}%)"
+                    asset_name = node_name.replace('_OUT', '').split(':')[0]
+                    if asset_name == 'resource_well_extractor':
+                        asset_name = 'resource_well_pressurizer'
 
-                asset_name = node_name.replace('_OUT', '').split(':')[0]
-                if asset_name == 'resource_well_extractor':
-                    asset_name = 'resource_well_pressurizer'
+                    elements.append({'data' : {
+                        'id'    : node_name,
+                        'label' : label,
+                        'image' : asset_data[asset_name].image_url
+                    }})
 
-                elements.append({'data' : {
-                    'id'    : node_name,
-                    'label' : label,
-                    'image' : asset_data[asset_name].image_url
-                }})
+                # Get connecting edges
+                for edge in planner.graph_edges:
+                    elements.append({'data' : {
+                        'source'    : edge.source_id,
+                        'target'    : edge.target_id,
+                        'weight'    : round(edge.rate,1)
+                    }})
 
-            # Get connecting edges
-            for edge in planner.graph_edges:
-                elements.append({'data' : {
-                    'source'    : edge.source_id,
-                    'target'    : edge.target_id,
-                    'weight'    : round(edge.rate,1)
-                }})
-
-            # Get raw materials
-            mats.append(html.P(f'\n{item_name} needs:'))
-            for root in planner.root_nodes:
-                root_node = planner.graph_nodes[root]
-                mats.append(html.P(f"{round(root_node.rate_produced,1)} {root_node.primary_item} per min"))
+                # Get raw materials
+                mats.append(html.P(f'\n{item_name} needs:'))
+                for root in planner.root_nodes:
+                    root_node = planner.graph_nodes[root]
+                    mats.append(html.P(f"{round(root_node.rate_produced,1)} {root_node.primary_item} per min"))
 
     return elements, layout, mats
 
